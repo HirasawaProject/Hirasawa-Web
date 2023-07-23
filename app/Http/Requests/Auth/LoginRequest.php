1f72ddc9f9cdca07bc0c4ad9ce00980b3a0a806c
Connor Graham <thecg1997@gmail.com>
2023-07-23 11:26:14 +0000

    Make sure system handles passwords MD5 hashed before bcrypt
    This is due to the osu! client sending an MD5 hash so this is what we have to work with

--- app/Http/Requests/Auth/LoginRequest.php
+++ app/Http/Requests/Auth/LoginRequest.php
@@ -41,7 +41,10 @@ class LoginRequest extends FormRequest
     {
         $this->ensureIsNotRateLimited();
 
-        if (! Auth::attempt($this->only('email', 'password'), $this->boolean('remember'))) {
+        $credentials = $this->only('email', 'password');
+        $credentials['password'] = md5($credentials['password']);
+        
+        if (! Auth::attempt($credentials, $this->boolean('remember'))) {
             RateLimiter::hit($this->throttleKey());
 
             throw ValidationException::withMessages([
