46d33bcfbf6ee33457abc12c33144f12c1259269
Connor Graham <thecg1997@gmail.com>
2023-07-31 21:22:30 +0000

    Update tests

--- tests/Feature/Auth/PasswordUpdateTest.php
+++ tests/Feature/Auth/PasswordUpdateTest.php
@@ -19,7 +19,7 @@ class PasswordUpdateTest extends TestCase
             ->actingAs($user)
             ->from('/profile')
             ->put('/password', [
-                'current_password' => 'password',
+                'current_password' => md5('password'),
                 'password' => 'new-password',
                 'password_confirmation' => 'new-password',
             ]);
@@ -28,7 +28,7 @@ class PasswordUpdateTest extends TestCase
             ->assertSessionHasNoErrors()
             ->assertRedirect('/profile');
 
-        $this->assertTrue(Hash::check('new-password', $user->refresh()->password));
+        $this->assertTrue(Hash::check(md5('new-password'), $user->refresh()->password));
     }
 
     public function test_correct_password_must_be_provided_to_update_password(): void
