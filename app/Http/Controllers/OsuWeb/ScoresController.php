3da9713ef409b3d266b3daced4bb51e6125eeeb5
Connor Graham <thecg1997@gmail.com>
2023-07-31 16:34:24 +0000

    Add authentication check to get scores route

--- app/Http/Controllers/OsuWeb/ScoresController.php
+++ app/Http/Controllers/OsuWeb/ScoresController.php
@@ -6,16 +6,24 @@ use App\Http\Controllers\Controller;
 use App\Models\Beatmap;
 use Illuminate\Http\Request;
 use App\Enums\Mode;
+use Illuminate\Support\Facades\Auth;
 
 class ScoresController extends Controller
 {
     function getScores(Request $request)
     {
-        $beatmap = Beatmap::where('hash', $request->input('c'))->first();
-        $mode = Mode::from($request->input('m'));
-        $scores = $beatmap ? $beatmap->getTop50($mode) : null;
-        $userScore = $beatmap ? $beatmap->scores()->where('rank', '<', 50)->inRandomOrder()->first() : null;
+        if (Auth::attempt([
+            'username' => $request->input('us'),
+            'password' => $request->input('ha')
+        ])) {
+            $beatmap = Beatmap::where('hash', $request->input('c'))->first();
+            $mode = Mode::from($request->input('m'));
+            $scores = $beatmap ? $beatmap->getTop50($mode) : null;
+            $userScore = $beatmap ? $beatmap->scores()->where('rank', '<', 50)->inRandomOrder()->first() : null;
 
-        return view('osuweb.leaderboard.index', compact('beatmap', 'mode', 'scores', 'userScore'));
+            return view('osuweb.leaderboard.index', compact('beatmap', 'mode', 'scores', 'userScore'));
+        } else {
+            return response('error:pass');
+        }
     }
 }
