b08fc5bdce61b532edd49c5153c4aa25f0e26d79
Connor Graham <thecg1997@gmail.com>
2023-08-10 22:34:07 +0000

    Apply correct global rank

--- app/Services/LeaderboardService.php
+++ app/Services/LeaderboardService.php
@@ -13,7 +13,7 @@ class LeaderboardService
     function processGlobalLeaderboard(Mode $mode)
     {
         $index = 1;
-        foreach (UserStats::where('mode', $mode->value)->orderBy('ranked_score')->get() as $userStat) {
+        foreach (UserStats::where('mode', $mode->value)->orderBy('ranked_score', 'desc')->get() as $userStat) {
             $userStat->rank = $index;
             $userStat->save();
             $index++;
