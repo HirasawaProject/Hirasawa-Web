e4905088bb34901b39ad9ead3be9b9a9f19ce71b
Connor Graham <thecg1997@gmail.com>
2023-08-10 23:56:30 +0000

    Add test for new score chart that beats older score

--- tests/Feature/Services/ChartServiceTest.php
+++ tests/Feature/Services/ChartServiceTest.php
@@ -77,4 +77,67 @@ class ChartServiceTest extends TestCase
             'onlineScoreId' => $score->id
         ], $chart[1]);
     }
+
+    public function testGetGenerateChartForNewBetterScore()
+    {
+        $beatmapSet = BeatmapSet::factory()->withBeatmaps()->create();
+        $beatmap = $beatmapSet->beatmaps->first();
+        $user = User::factory()->withStats()->create();
+
+        $userStatsBefore = $user->getUserStats(Mode::OSU);
+
+        $scoreBefore = Score::factory()->create([
+            'beatmap_id' => $beatmap->id,
+            'user_id' => $user->id,
+            'score' => 1000,
+        ]);
+
+        $scoreAfter = Score::factory()->create([
+            'beatmap_id' => $beatmap->id,
+            'user_id' => $user->id,
+            'score' => 2000,
+        ]);
+
+        
+
+        $userStatsAfter = $user->getUserStats(Mode::OSU);
+        
+        $chart = $this->chartService->generateCharts($user, $beatmap, $scoreBefore, $scoreAfter, $userStatsBefore, $userStatsAfter);
+
+        $this->assertCount(2, $chart);
+
+        $this->assertEquals([
+            'chartId' => 'beatmap',
+            'chartName' => 'Beatmap Ranking',
+            'chartUrl' => config('osu_url') . "/b/{$beatmap->osu_id}",
+            'rankedScoreBefore' => $scoreBefore->score,
+            'rankedScoreAfter' => $scoreAfter->score,
+            'maxComboBefore' => $scoreBefore->combo,
+            'maxComboAfter' => $scoreAfter->combo,
+            'accuracyBefore' => $scoreBefore->accuracy,
+            'accuracyAfter' => $scoreAfter->accuracy,
+            'ppBefore' => 0,
+            'ppAfter' => 0,
+            'rankBefore' => $scoreBefore->rank,
+            'rankAfter' => $scoreAfter->rank,
+            'onlineScoreId' => $scoreAfter->id
+        ], $chart[0]);
+
+        $this->assertEquals([
+            'chartId' => 'overall',
+            'chartName' => 'Overall Ranking',
+            'chartUrl' => config('osu_url') . "/u/{$user->id}",
+            'rankedScoreBefore' => $userStatsBefore->ranked_score,
+            'rankedScoreAfter' => $userStatsAfter->ranked_score,
+            'totalScoreBefore' => $userStatsBefore->total_score,
+            'totalScoreAfter' => $userStatsAfter->total_score,
+            'accuracyBefore' => $userStatsBefore->accuracy,
+            'accuracyAfter' => $userStatsAfter->accuracy,
+            'ppBefore' => 0,
+            'ppAfter' => 0,
+            'rankBefore' => $userStatsBefore->rank,
+            'rankAfter' => $userStatsAfter->rank,
+            'onlineScoreId' => $scoreAfter->id
+        ], $chart[1]);
+    }
 }
