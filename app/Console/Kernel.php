4b05654a90bb0e61b086a6c7f75a8f941143c09c
Connor Graham <thecg1997@gmail.com>
2023-08-21 20:41:42 +0000

    Run rank history processing each day

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -13,6 +13,7 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule): void
     {
         // $schedule->command('inspire')->hourly();
+        $schedule->command('tasks:rank-history:process')->dailyAt('00:00');
     }
 
     /**
