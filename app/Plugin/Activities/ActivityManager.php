3c4d0fdaef6186cc96736c9fbd275d24329afd2a
Connor Graham <thecg1997@gmail.com>
2023-08-22 19:28:30 +0000

    Add support to attach an activity to a user

--- app/Plugin/Activities/ActivityManager.php
+++ app/Plugin/Activities/ActivityManager.php
@@ -21,4 +21,25 @@ class ActivityManager
 
         return $this->registeredActivities[$activityKey]->build($activity);
     }
+
+    function attachActivity(User $user, String $activityKey, array $params = []): UserActivity
+    {
+        if (!array_key_exists($activityKey, $this->registeredActivities)) {
+            throw new \Exception("Activity not registered");
+        }
+
+        $this->registerActivity[$activityKey]->getRequiredParams()->each(function ($requiredParam) use ($params) {
+            if (!array_key_exists($requiredParam, $params)) {
+                throw new \Exception("Missing required parameter: $requiredParam");
+            }
+        });
+
+        $activity = new UserActivity();
+        $activity->user_id = $user->id;
+        $activity->key = $activityKey;
+        $activity->params = $params;
+        $activity->save();
+
+        return $activity;
+    }
 }
