03bfb0544d223bf79ee1d3cb7016fabfca0b79a4
Connor Graham <thecg1997@gmail.com>
2023-08-22 19:34:46 +0000

    Fix copy-paste error

--- app/Facades/ActivityManager.php
+++ app/Facades/ActivityManager.php
@@ -4,7 +4,7 @@ namespace App\Facades;
 
 use Illuminate\Support\Facades\Facade;
 
-class PluginManager extends Facade
+class ActivityManager extends Facade
 {
     protected static function getFacadeAccessor()
     {
